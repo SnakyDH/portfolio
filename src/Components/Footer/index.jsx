73d4fe807d019617a0e54842298028ff16cb751a
SnakyDH <skdanielg5@gmail.com>
2023-01-28 21:41:51 -0500

    [TODO] fix svg of Footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -4,40 +4,53 @@ function Footer() {
   return (
     <>
       <hr className="bg-green-600 p-3" />
-      <footer className={"bg-blue-section h-80"}>
+      {
+        //todo fix background image error
+      }
+      <footer className={"text-white h-full w-full"}>
         <div>
-          <h2
-            className={"text-white text-center text-4xl font-extrabold pt-10"}
-          >
+          <h2 className={"text-center text-4xl font-extrabold pt-10"}>
             Daniel García Romero
           </h2>
         </div>
         <div className={"flex justify-around pt-10"}>
           <div>
             <img
-              className={"rounded-full w-16 sm:w-28 invert hover:invert-0"}
-              src={"/public/socialMediaSVG/github.svg"}
+              className={
+                "rounded-full w-16 sm:w-28 invert hover:invert-0 hover:shadow-xl hover:shadow-slate-500"
+              }
+              src={"/socialMediaSVG/github.svg"}
               alt={"github"}
             />
-            <p className={"py-3 text-white text-center"}>GitHub</p>
+            <p className={"py-3 text-center"}>GitHub</p>
           </div>
           <div>
             <img
-              className={"rounded-full w-16 sm:w-28 invert hover:invert-0"}
-              src="/public/socialMediaSVG/email.svg"
-              alt={"Linkedin"}
+              className={
+                "bg-slate-200 rounded-full w-16 sm:w-28 invert hover:invert-0 hover:shadow-xl hover:shadow-rose-600"
+              }
+              src="/socialMediaSVG/email.svg"
+              alt={"email"}
             />
-            <p className={"py-3 text-white text-center"}>LinkedIn</p>
+            <p className={"py-3 text-center"}>Email</p>
           </div>
           <div>
             <img
-              className={"rounded-full w-16 sm:w-28 invert hover:invert-0"}
-              src="/public/socialMediaSVG/linkedin.svg"
+              className={
+                "rounded-full w-16 sm:w-28 invert hover:invert-0 hover:shadow-xl hover:shadow-blue-500"
+              }
+              src="/socialMediaSVG/linkedin.svg"
               alt={"Linkedin"}
             />
-            <p className={"py-3 text-white text-center"}>Email</p>
+            <p className={"py-3 text-center font-bold rounded-lg"}>LinkedIn</p>
           </div>
         </div>
+        <p className="text-center py-5">
+          Made With ❤ by
+          <a href="https://github.com/SnakyDH/portfolio" target={"_blank"}>
+            {" SnakyDH"}
+          </a>
+        </p>
       </footer>
     </>
   );
