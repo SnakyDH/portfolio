e078682b88837f2d6cd25ab2887eb152e8e53e8a
SnakyDH <skdanielg5@gmail.com>
2024-02-04 16:11:29 -0500

    feat: add tailwind colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      primary: "#2BA764",
+      onPrimary: "#FFFFFF",
+      secondary: "#4828C9",
+      onSecondary: "#FFFFFF",
+      background: "#0A0822",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
