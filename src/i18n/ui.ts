ee7b718bf12dd5533d10bec81332435d7048e6d2
SnakyDH <skdanielg5@gmail.com>
2025-01-01 17:33:11 -0500

    feat(lang): config i18n using en by default

---
+++ src/i18n/ui.ts
@@ -0,0 +1,17 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const defaultLang = "en";
+export const showDefaultLang = false;
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+  },
+  es: {
+    "nav.home": "Inicio",
+    "nav.about": "Acerca de",
+  },
+} as const;
