3cdaebefc023ef479640191c07f08bc5997a077d
SnakyDH <skdanielg5@gmail.com>
2025-01-01 20:29:49 -0500

    feat(aboutMe): add some assets and words for this section

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -7,6 +7,7 @@ export const defaultLang = "en";
 export const showDefaultLang = false;
 export const ui = {
   en: {
+    portfolio: "Portfolio",
     "nav.home": "Home",
     "nav.about": "About Me",
     "nav.experience": "Experience",
@@ -14,10 +15,16 @@ export const ui = {
     "nav.skills": "Skills",
     // Developer information
     "dev.name": "Daniel Garcia Romero",
+    "dev.title.frontend": "Frontend Developer",
+    "dev.title.mobile": "Mobile Developer",
+    "dev.title.fullstack": "Fullstack Developer",
+    "dev.linkedin": "LinkedIn",
+    "dev.github": "GitHub",
     // Skills Section
     "skills.title": "Skills",
   },
   es: {
+    portfolio: "Portafolio",
     "nav.home": "Inicio",
     "nav.about": "Sobre mí",
     "nav.experience": "Experiencia",
@@ -25,6 +32,9 @@ export const ui = {
     "nav.skills": "Habilidades",
     // Developer information
     "dev.name": "Daniel García Romero",
+    "dev.title.frontend": "Desarrollador Frontend",
+    "dev.title.mobile": "Desarrollador Móvil",
+    "dev.title.fullstack": "Desarrollador Fullstack",
     // Skills section
     "skills.title": "Habilidades",
   },
